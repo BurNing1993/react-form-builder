8793af116f3e7d51a12cfe6c32239e081ba4b71e
Joey <786678257@qq.com>
2021-11-01 11:40:42 +0800

    style💄:  form list

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,8 @@
-import { Card, Col, Row } from 'antd'
+import { Card, Col, Empty, Row } from 'antd'
 import React, { memo } from 'react'
 import { Link } from 'react-router-dom'
 import { useRecoilValue } from 'recoil'
+import dayjs from 'dayjs'
 import { formListState } from '../form/store/selector'
 
 const Home: React.FC = () => {
@@ -26,18 +27,25 @@ const Home: React.FC = () => {
       </Card>
       <Card title="Recent" bordered={false} className="mt-8">
         <Row gutter={[20, 20]}>
-          {formList.map((f) => (
-            <Col key={f.id} xs={12} sm={8} md={6} lg={4} xl={3}>
-              <Link
-                to={'/f/' + f.id}
-                className="border border-gray-600  block cursor-pointer text-center py-6 hover:border-blue-600"
-                title={'go No.' + f.id + ' ' + f.name}
-              >
-                <div className="font-bold text-xl">No.{f.id}</div>
-                <div>{f.name}</div>
-              </Link>
-            </Col>
-          ))}
+          {formList.length ? (
+            formList.map((f) => (
+              <Col key={f.id} xs={12} sm={8} md={6} lg={4} xl={3}>
+                <Link
+                  to={'/f/' + f.id}
+                  className="border border-gray-600  block cursor-pointer py-4 text-center hover:border-blue-600"
+                  title={'go No.' + f.id + ' ' + f.name}
+                >
+                  <div className="font-bold text-xl">No.{f.id}</div>
+                  <div className="text-base py-2">{f.name}</div>
+                  <div className="text-xs">
+                    {dayjs(f.createAt).format('YYYY-MM-DD HH:mm:ss')}
+                  </div>
+                </Link>
+              </Col>
+            ))
+          ) : (
+            <Empty className="mx-auto" />
+          )}
         </Row>
       </Card>
     </main>
